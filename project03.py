629b898ce9dcf9339ad0886fef3de885745ea643
jnaiburg <32399054+jnaiburg@users.noreply.github.com>
2020-02-12 17:30:55 -0500

    Add files via upload

---
+++ project03.py
@@ -0,0 +1,67 @@
+'''
+Created on Feb 5, 2020
+
+@author: Joseph Naiburg
+'''
+
+from prettytable import PrettyTable
+
+
+
+valid = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS",
+         "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE",
+         "HEAD", "TRLR", "NOTE"]
+
+
+def isValid(s):
+    if s in valid:
+        return True
+    else:
+        return False
+
+def run():
+    itable = PrettyTable()
+    ftable = PrettyTable()
+    f = open("Project01.ged", "r")
+    itable.field_names = ['ID', 'Name', 'Gender', 'Birth Date', 'Death Date']
+    indi = ''
+    name = ''
+    sex = ''
+    bdate = ''
+    ddate = 'N/A'
+    
+    for x in f:
+        temp = x.split()
+        if temp[0] == '0':
+            
+            if(len(temp) > 2):
+                if temp[2] == 'INDI':
+                    if indi != '':
+                        itable.add_row([indi, name, sex, bdate, ddate])
+                        ddate = 'N/A'
+                    indi = temp[1]
+         
+        if temp[0] == '1':
+            if temp[1] == 'NAME':
+                name = temp[2] + ' ' + temp[3]
+            if temp[1] == 'SEX':
+                sex = temp[2]
+            if temp[1] == 'BIRT':
+                dtype = 'birth'
+            if temp[1] == 'DEAT':
+                dtype = 'death'
+                
+        if temp[0] == '2':
+            if temp[1] == 'DATE':
+                dtemp = temp[2] + ' ' + temp[3] + ' ' + temp[4]
+                if dtype == 'birth':
+                    bdate = dtemp
+                if dtype == 'death':
+                    ddate = dtemp
+                
+    print(itable)
+            
+        
+run()
+        
+ 
