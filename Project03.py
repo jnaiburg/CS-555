81a6ddbce4e3026f1c044f52008b70d36067da20
vincenzosusi <43451681+vincenzosusi@users.noreply.github.com>
2020-02-12 22:00:01 -0500

    Updated project file

---
+++ Project03.py
@@ -0,0 +1,128 @@
+'''
+Created on Feb 5, 2020
+@author: Joseph Naiburg
+'''
+
+from prettytable import PrettyTable
+
+def run():
+    
+    #initialize the individual table and family table
+    itable = PrettyTable()
+    ftable = PrettyTable()
+    #open the ged file
+    f = open("gedcom.ged", "r")
+    #make the columns for the itable
+    itable.field_names = ['ID', 'Name', 'Gender', 'Birth Date', 'Age', 'Alive', 'Death Date', 'Child', 'Spouse']
+    ftable.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Wife ID', 'Children']
+    #initialize a bunch of variables
+    indi = ''
+    name = ''
+    sex = ''
+    bdate = ''
+    ddate = 'N/A'
+    age = ''
+    alive = 'True'
+    child = 'N/A'
+    spouse = 'N/A'
+    isindi = True
+    
+    famid = ''
+    mdate='N/A'
+    divorced='N/A'
+    husbid=''
+    wifeid=''
+    children='N/A'
+    
+    
+    
+    byear = 0
+    
+    #iterate through each line
+    for x in f:
+        #split by space
+        temp = x.split()
+        #id 0
+        if temp[0] == '0':
+            
+            if(len(temp) > 2):
+                #new person
+                if temp[2] == 'INDI':
+                    if indi != '':
+                        #add the row of info for the previous guy to the itable
+                        itable.add_row([indi, name, sex, bdate, age, alive, ddate, child, spouse])
+                        ddate = 'N/A'
+                        alive = 'True'
+                        child = 'N/A'
+                        spouse = 'N/A'
+                        #get the new id for the next guy
+                    indi = temp[1]
+                if temp[2] == 'FAM':
+                    if isindi:
+                        itable.add_row([indi, name, sex, bdate, age, alive, ddate, child, spouse])
+                        isindi = False
+                    else:
+                        ftable.add_row([famid, mdate, divorced, husbid, wifeid, children])
+                    famid = temp[1] 
+                        
+        #id 0
+        if temp[0] == '1':
+            #the persons name
+            if temp[1] == 'NAME':
+                name = temp[2] + ' ' + temp[3]
+            #their gender
+            if temp[1] == 'SEX':
+                sex = temp[2]
+            #make a variable to know that the next line is the birthday
+            if temp[1] == 'BIRT':
+                dtype = 'birth' 
+            #same but with death date
+            if temp[1] == 'DEAT':
+                dtype = 'death'
+            
+            if temp[1] == 'MARR':
+                dtype = 'marr'
+            if temp[1] == 'DIV':
+                dtype = 'divorce'
+            
+            if temp[1] == 'FAMC':
+                child = temp[2]
+            if temp[1] == 'FAMS':
+                spouse = temp[2]
+            
+            if temp[1] == 'HUSB':
+                husbid = temp[2]
+                        
+            if temp[1] == 'WIFE':
+                wifeid = temp[2]
+            if temp[1] == 'CHIL':
+                children = temp[2]
+        
+        #id 2 
+        if temp[0] == '2':
+            #figuring out death vs birth date and storing it to a variable
+            if temp[1] == 'DATE':
+                dtemp = temp[2] + ' ' + temp[3] + ' ' + temp[4]
+                if dtype == 'birth':
+                    bdate = dtemp
+                    byear = int(temp[4])
+                    #age in 2020
+                    age = 2020 - byear
+                if dtype == 'death':
+                    alive = 'False'
+                    ddate = dtemp
+                    #age at time of death
+                    age = int(temp[4]) - byear
+                if dtype == 'marr':
+                    mdate = dtemp
+                if dtype == 'divorce':
+                    divorced = dtemp
+    #add the last row and print stuff
+    ftable.add_row([famid, mdate, divorced, husbid, wifeid, children])
+    print("Individuals")
+    print(itable)
+    print("Families")
+    print(ftable)
+            
+        
+run()
