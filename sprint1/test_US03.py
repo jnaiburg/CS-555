84c791083cba2c760ebfe6229c3c97d4c3ab7601
vincenzosusi <43451681+vincenzosusi@users.noreply.github.com>
2020-02-25 15:30:47 -0500

    Added tests for US06

--- sprint1/test_US03.py
+++ sprint1/test_US03.py
@@ -9,16 +9,19 @@ import Project03
 
 class Test(unittest.TestCase):
 
-    def test01(self):
+    def test_US03(self):
         self.assertFalse(Project03.US03("9 MAY 2000", "10 FEB 1900"))
-    def test02(self):
         self.assertTrue(Project03.US03("9 MAY 2000", "10 FEB 2004"))
-    def test03(self):
         self.assertTrue(Project03.US03("15 SEP 1972", "10 OCT 2002"))
-    def test04(self):
         self.assertFalse(Project03.US03("27 JAN 1990", "26 JAN 1990"))
-    def test05(self):
         self.assertTrue(Project03.US03("1 DEC 1975", "1 DEC 1975"))
+    
+    def test_US06(self):
+        self.assertFalse(Project03.US06("9 MAY 2000", "10 FEB 1900", "24 MAR 1905"))
+        self.assertTrue(Project03.US06("9 MAY 2000", "10 FEB 2004", "24 MAR 2005"))
+        self.assertTrue(Project03.US06("15 SEP 1972", "10 OCT 2002", "30 AUG 1980"))
+        self.assertFalse(Project03.US06("27 JAN 1990", "26 JAN 1990", "22 JAN 1990"))
+        self.assertTrue(Project03.US06("1 DEC 1975", "1 DEC 1975", "1 DEC 1975"))
         
 if __name__ == "__main__":
     unittest.main()
